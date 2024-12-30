0a756ab00b96c24e8aecaa326f45c8e664d5fd4d
Edwin Cardenas <eacardenase@gmail.com>
2024-12-30 14:43:14 -0500

    refactor: store person struct on the heap and manipulate its pointer

--- BMI/main.c
+++ BMI/main.c
@@ -6,32 +6,35 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     float heightInMeters;
     int weightInKilos;
 } Person;
 
-float bodyMassIndex(Person p)
+float bodyMassIndex(Person *p)
 {
-    return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
+    return p->weightInKilos / (p->heightInMeters * p->heightInMeters);
 }
 
 int main(int argc, const char * argv[]) {
-    Person edwin;
-    edwin.heightInMeters = 1.8;
-    edwin.weightInKilos = 96;
+    // Allocate memory for one Person struct
+    Person *edwin = (Person *)malloc(sizeof(Person));
     
-    Person aaron;
-    aaron.heightInMeters = 1.97;
-    aaron.weightInKilos = 84;
+    // Fill in two members of the struct
+    edwin->heightInMeters = 1.8;
+    edwin->weightInKilos = 96;
     
-    float bmi;
-    bmi = bodyMassIndex(edwin);
-    printf("edwin has a BMI of %.2f\n", bmi);
+    // Print out the BMI of the original Person
+    float edwinBmi = bodyMassIndex(edwin);
+    printf("Edwin has a BMI of %.2f\n", edwinBmi);
     
-    bmi = bodyMassIndex(aaron);
-    printf("aaron has a BMI of %.2f\n", bmi);
+    // Let the memory be recycled
+    free(edwin);
+    
+    // Forget where it was
+    edwin = NULL;
     
     return 0;
 }
