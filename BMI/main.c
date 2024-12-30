9088591840c0485c76fe1c67c94119f80e005916
Edwin Cardenas <eacardenase@gmail.com>
2024-12-30 13:55:57 -0500

    feat: add person struct declaration

--- BMI/main.c
+++ BMI/main.c
@@ -7,8 +7,24 @@
 
 #include <stdio.h>
 
+struct Person {
+    float heightInMeters;
+    int weightInKilos;
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    struct Person edwin;
+    edwin.heightInMeters = 1.8;
+    edwin.weightInKilos = 96;
+    
+    struct Person aaron;
+    aaron.heightInMeters = 1.97;
+    aaron.weightInKilos = 84;
+    
+    printf("edwin is %.2f meters tall\n", edwin.heightInMeters);
+    printf("edwin weighs %d kilograms\n", edwin.weightInKilos);
+    printf("aaron is %.2f meters tall\n", aaron.heightInMeters);
+    printf("aaron weighs %d kilograms\n", aaron.weightInKilos);
+    
     return 0;
 }
